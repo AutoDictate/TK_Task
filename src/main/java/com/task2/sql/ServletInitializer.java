548b444ef581fa85a8a9ec319c5c550c88b4b269
AutoDictate <jayasurya0206@gmail.com>
2024-03-05 13:15:52 +0530

    Maven Build for War file Successful

---
+++ src/main/java/com/task2/sql/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.task2.sql;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SqlApplication.class);
+	}
+
+}
