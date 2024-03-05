b419820f3459eace038603ac2d79fbfef5693372
AutoDictate <jayasurya0206@gmail.com>
2024-03-05 15:19:42 +0530

    Properties values printed in console using annotations

---
+++ src/main/java/com/task2/sql/PropsValues.java
@@ -0,0 +1,28 @@
+package com.task2.sql;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@Data
+
+public class PropsValues{
+	
+	@Value("${spring.datasource.username}") 
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	
+}
--- src/main/java/com/task2/sql/SqlApplication.java
+++ src/main/java/com/task2/sql/SqlApplication.java
@@ -1,7 +1,9 @@
 package com.task2.sql;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SqlApplication {
@@ -9,5 +11,12 @@ public class SqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SqlApplication.class, args);
 	}
-
+	
+	@Bean
+    public CommandLineRunner printVal(PropsValues propsValues) {
+        return args ->{
+            String username = propsValues.getUsername();
+            System.out.println("Username: " + username);
+        };
+    }
 }
