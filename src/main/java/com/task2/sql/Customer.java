cfe8030152925d45f848649c3778028e14abc146
AutoDictate <jayasurya0206@gmail.com>
2024-03-05 13:39:52 +0530

    Customer Class created and values inserted through java class

---
+++ src/main/java/com/task2/sql/Customer.java
@@ -0,0 +1,26 @@
+package com.task2.sql;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name ="Customer")
+public class Customer{
+	
+	@Id
+	@Column(name ="id")
+	private int customer_id;
+	
+	@Column(name ="name")
+	private String customer_name;
+	
+	@Column(name ="address")
+	private String customer_address;
+	
+	@Column(name ="age")
+	private int customer_age;
+}
