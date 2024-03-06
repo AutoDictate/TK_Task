bb0e10dba7a754097f547394bd87a7b1b676f48b
AutoDictate <jayasurya0206@gmail.com>
2024-03-06 14:15:23 +0530

    CRUD operation performed using CrudRepository

--- src/main/java/com/task2/sql/Repository/CustomerRepository.java
+++ src/main/java/com/task2/sql/Repository/CustomerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.task2.sql.CustomerEntity.Customer;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
+public interface CustomerRepository extends CrudRepository<Customer, Integer>{
 	
 }
